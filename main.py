351074779d5fddde8fe92939e2f150c25fc62463
Nuvix <a51menrasy@gmail.com>
2026-04-28 13:41:03 -0600

    agregando comentario waza

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from funcion_n import suma, resta, multiplicacion
 print("Suma:", suma(10, 5))
 print("Resta:", resta(10, 5))
 print("Multiplicación:", multiplicacion(10, 5))
-#wazaaaaaaaaaaaaaa
+#wazaaaaaaaaaaaaaaaaa
